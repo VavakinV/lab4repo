dc9817569fadd8f734b7be473b7d7a740c7a5259
VavakinV <vladvavakin@gmail.com>
2024-11-21 23:35:39 +0300

    added commentaries

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,8 +5,8 @@ int main() {
     std::string name;
     
     std::cout << "Введите ваше имя: ";
-    std::cin >> name;
+    std::cin >> name; // Получение имени пользователя
     
-    std::cout << "Hello world from " << name << std::endl;
+    std::cout << "Hello world from " << name << std::endl; // Вывод в стандартный поток
     return 0;
 }
