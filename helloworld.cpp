ad940815819898028e72e33d6d99e41a54dd65f1
VavakinV <vladvavakin@gmail.com>
2024-11-21 23:27:18 +0300

    removed using namespace std; from helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-    string name;
+    std::string name;
     
-    cout << "Введите ваше имя: ";
-    cin >> name;
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
     
-    cout << "Hello world from " << name << endl;
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
